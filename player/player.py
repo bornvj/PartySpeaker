eb063a294db57d5541a2373e551d1eaeffab043e
Titouan Guionneau <titouan.guionneau@epita.fr>
2024-11-02 14:01:43 +0100

    fix youtube inputs

--- player/player.py
+++ player/player.py
@@ -6,24 +6,30 @@ import pygame
 import subprocess
 from traductor import write_titles
 
-ydl_opts = {
-        'format': 'bestaudio/best',  # Télécharger la meilleure qualité audio
-        'extractaudio': True,         # Extraire uniquement l'audio
-#        'audioformat': 'mp3',         # Convertir au format MP3
-        'outtmpl': '%(title)s.%(ext)s.audio',  # Nommer le fichier selon le titre de la vidéo
-#        'postprocessors': [{           # Utiliser un post-processeur pour convertir en MP3
-#            'key': 'FFmpegExtractAudio',
-#            'preferredcodec': 'mp3',
-#            'preferredquality': '192',
-#        }],
-        'enable_file_urls': True, # To add another vulnerability hehe
-    }
+ydl_opts_base = {
+    'format': 'bestaudio/best',  # Télécharger la meilleure qualité audio
+    'extractaudio': True,         # Extraire uniquement l'audio
+    'outtmpl': '%(title)s.%(ext)s.audio',  # Nommer le fichier selon le titre de la vidéo
+    'enable_file_urls': True,
+}
+
+ydl_opts_yt = {
+    'format': 'bestaudio/best',  # Télécharger la meilleure qualité audio
+    'extractaudio': True,         # Extraire uniquement l'audio
+    'audioformat': 'mp3',         # Convertir au format MP3
+    'outtmpl': '%(title)s.%(ext)s',  # Nommer le fichier selon le titre de la vidéo
+    'postprocessors': [{           # Utiliser un post-processeur pour convertir en MP3
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
 
 file = 'soundQueue/queue.txt'
 currentLine = 0
 pygame.mixer.init()
 
-garbage_files = glob.glob("*.audio")
+garbage_files = glob.glob("*.audio") + glob.glob("*.mp3")
 for f in garbage_files:
     os.remove(f)
 
@@ -42,10 +48,12 @@ while True:
             url = lines[currentLine].strip()
             currentLine += 1
 
+            ydl_opts = ydl_opts_yt if "youtube" in url else ydl_opts_base
+
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 try:
                     ydl.download([url])
-                    audio_files = glob.glob("*.audio")
+                    audio_files = glob.glob("*.audio") + glob.glob("*.mp3")
 
                     if len(audio_files) > 0:
                         audio_file = audio_files[0]
@@ -66,7 +74,3 @@ while True:
             time.sleep(3)
 
 exit()
-
-url = 'https://youtu.be/68ugkg9RePc?si=BW78VU_r8_fWNkaI'
-
-
